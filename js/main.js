983a4a639b802941510a9db83a25e496926ddfba
Aleksandar <apetrovic321@gmail.com>
2023-10-31 14:18:58 +0100

    Add functions for creating a new grid based on user input and clearing the grid
    size and clear buttons are working as intended

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
+// CREATE GRID BASED ON USER INPUT
 const gridContainer = document.querySelector("#grid-container");
-const num = 10;
-
-// create Grid based on the num input
 const createGrid = function (num) {
   // create a single row
   const createRow = function (num) {
@@ -24,4 +22,30 @@ const createGrid = function (num) {
   }
 };
 
-createGrid(num); // call the function
+// GRID SIZE USER INPUT
+// it can all go inside one function, but this way is more modular and there is no need to call the funciton to activate the event listener on the sizeBtn
+const sizeInput = () => {
+  num = prompt("Select a number from 1 to 100");
+  clearGrid(); // first clear the existing grid
+  createGrid(num); // create new grid based on user input
+};
+// add event listener to sizeBtn
+const sizeBtn = document.querySelector(".grid-size");
+sizeBtn.addEventListener("click", () => {
+  sizeInput();
+});
+
+// RESET GRID
+// If I add clearGrid() directly as an event listener it won't run by itself in the sizeInput function without the click event, so it has to be declared separately
+const clearGrid = () => {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+};
+// add the event listener to clearBtn
+const clearBtn = document.querySelector(".reset");
+clearBtn.addEventListener("click", () => {
+  clearGrid();
+});
+
+createGrid(6); // optional: set the initial grid when the page is loaded - it will be overwritten when the sizeInput is entered
